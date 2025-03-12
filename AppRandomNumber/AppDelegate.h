791a63c8242998db7d0ac10c0cdbac6a19c658fa
MiriamASanchezC <miriamaliciasanchezcervantes@gmail.com>
2025-03-12 17:16:06 -0600

    Initial Commit

---
+++ AppRandomNumber/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  AppRandomNumber
+//
+//  Created by Miriam Sanchez on 12/03/25.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
